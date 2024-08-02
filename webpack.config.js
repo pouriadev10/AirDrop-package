abc7a7d9650f96a85e5e0ca4ed1baf6e33a06e14
H.barzin <barzinprograms@gmail.com>
2024-08-02 11:48:26 +0330

    structure updated

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    library: 'AirdropSDK',
+    libraryTarget: 'umd',
+    globalObject: 'this'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom'
+  }
+};
